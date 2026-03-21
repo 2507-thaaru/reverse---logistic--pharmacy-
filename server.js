6f0d05c5b2985c2dc4ee6084ff15f3acfb47c386
Priyanka1846 <anupriyaani1814@gmail.com>
2026-03-21 15:30:24 +0530

    fix: add root route

--- server.js
+++ server.js
@@ -46,6 +46,11 @@ app.post("/reset", (req, res) =>
     res.json({ message: "Blockchain reset" });
 });
 
+app.get("/", (req, res) =>
+{
+    res.send("Pharma Reverse Logistics Tracker Bacnend is Running!");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>
